4763aa55025dd2be2f896ea1a6810f897fa1282e
Haziel Ortiz <ya-yo22124@outlook.com>
2024-01-28 15:32:22 -0600

    Upd: CV download button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	important:true,
+	important: true,
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+			"animation": {
+				"background-shine": "background-shine 2s linear infinite"
+			},
+			"keyframes": {
+				"background-shine": {
+					"from": {
+						"backgroundPosition": "0 0"
+					},
+					"to": {
+						"backgroundPosition": "-200% 0"
+					}
+				}
+			}
+
+		},
 	},
 	plugins: [
 		require('tailwindcss-animated')
